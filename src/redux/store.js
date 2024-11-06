337983169ab52021b2d484f7ab6e1a4ba445182c
Kyrylo <kirilzhm@gmail.com>
2024-11-06 18:26:32 +0100

    the end of homework

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,23 +1,12 @@
-import { configureStore } from '@reduxjs/toolkit';
-import { persistStore, persistReducer } from 'redux-persist';
-import storage from 'redux-persist/lib/storage';
+import { configureStore, combineReducers } from '@reduxjs/toolkit';
 import contactsReducer from './contactsSlice';
 import filtersReducer from './filtersSlice';
-import { combineReducers } from 'redux';
-
-const contactsPersistConfig = {
-  key: 'contacts',
-  storage,
-  whitelist: ['items'],
-};
 
 const rootReducer = combineReducers({
-  contacts: persistReducer(contactsPersistConfig, contactsReducer),
+  contacts: contactsReducer,
   filters: filtersReducer,
 });
 
 export const store = configureStore({
   reducer: rootReducer,
 });
-
-export const persistor = persistStore(store);
